2816125d207be90d495e6ebfce859bc0dfe89f5b
rlp1990 <rlp1990@gmail.com>
2021-11-29 11:12:32 +0100

    Testing Add and Substract from calculator service

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,14 +1,31 @@
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+
 describe('CalculatorService', () => {
 
     it('should add two numbers', () => {
 
-        pending();
+        // Instantiate calculatorService to test the Component
+        const calculator = new CalculatorService(new LoggerService());
+
+        // Operation Add with the service
+        const result = calculator.add(2, 2);
+
+        // Testing service Add functionality
+        expect(result).toBe(4);
 
     });
 
     it('should substract two numbers', () => {
 
-        fail();
+        // Instantiate calculatorService to test the Component
+        const calculator = new CalculatorService(new LoggerService());
+
+        // Operation Substract with the service
+        const result = calculator.subtract(2, 2);
+
+        // Testing service Add functionality
+        expect(result).toBe(0, 'unexpected substraction result');
         
     });
 });
